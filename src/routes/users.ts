3ed98f03dfa252279846da8df99cc2be0bb44847
Erik Michael Larson <erik.larson@codercamps.com>
2017-09-05 01:49:49 -0700

    built a functional api for users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -11,4 +11,58 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+  let userId = req.params.id;
+
+  User.findOne({ _id: userId }).then((foundUser) => {
+    res.json(foundUser);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  let newUser = new User();
+
+  newUser.dateCreated = new Date();
+  switch (req.body.userType) {
+    case 'user':
+      newUser.userType = req.body.userType;
+      break;
+    case 'artist':
+      newUser.userType = req.body.userType;
+      break;
+    default:
+      newUser.userType = 'user';
+      break;
+  }
+  newUser.firstName = req.body.firstName;
+  newUser.lastName = req.body.lastName;
+  newUser.email = req.body.email;
+  newUser.username = req.body.username;
+  newUser.password = req.body.password;
+  newUser.profilePicture = req.body.profilePicture;
+  newUser.bio = req.body.bio;
+
+  newUser.save().then((createdUser) => {
+    res.json(createdUser);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  let userId = req.params.id;
+
+  User.findOne({ _id: userId }).then((foundUser) => {
+    User.remove(foundUser).then((deletedUser) => {
+      res.json(deletedUser);
+    }).catch((err) => {
+      res.json(err);
+    });
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
 export default router;
